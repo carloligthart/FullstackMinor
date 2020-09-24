6f62dd8f674227d18bbebd2469bd972cf1fedc90
Carlo Ligthart <carloligthart@gmail.com>
2020-09-24 21:00:22 +0200

    Initialized the project

---
+++ simpleController/src/test/java/com/assignments1_2/simpleController/SimpleControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignments1_2.simpleController;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
